c034fcbced580805856dc1648fdcb9c80d9d75a4
Hector <HMoragues3@gmail.com>
2023-05-14 21:11:24 +0200

    cambios registro

--- registro.php
+++ registro.php
@@ -1,26 +1,49 @@
 <?php 
 include "config/requires.php";
 
+if (isset($_POST["accion"])) {
+  $accion = $_POST["accion"];
+  if(isset($_POST["correo"])){
+    //comprobar si existe el correo en la bd
+    //...
+    if(true){//si existe
+      echo "existe";
+    }else{
+      echo "no_existe";
+    }
+
+  }else{
+    var_dump($_POST);
+  }
+  exit();
+}
+
+if(!empty($_POST["nombre"]) && !empty($_POST["correo"]) && !empty($_POST["password"]) && !empty($_POST["password_confirm"])){
+  //echo "SUBMIT!";
+  // Insertar usuario en la base de datos
+  //$controladorUsuarios = new ControladorUsuarios($conexion);
+  //$resultado = $controladorUsuarios->procesarRegistro();
+  var_dump($_POST);
+  
+  if($resultado){
+      // El usuario ha sido insertado correctamente
+  }else{
+      // Error al insertar el usuario en la base de datos
+  }
+
+}
+
+
+
+
+
 include 'vistas/header.php'; 
 $modeloPerfilesUsuario = new ModeloPerfilesUsuario($conexion);
 $municipios = $modeloPerfilesUsuario->obtenerMunicipios();
-  //var_dump($municipios);
-  //exit();
-  // Validar datos
-  if(!empty($_POST["nombre"]) && !empty($_POST["correo"]) && !empty($_POST["password"]) && !empty($_POST["password_confirm"])){
-    //echo "SUBMIT!";
-    // Insertar usuario en la base de datos
-    //$controladorUsuarios = new ControladorUsuarios($conexion);
-    //$resultado = $controladorUsuarios->procesarRegistro();
-    var_dump($_POST);
-    
-    if($resultado){
-        // El usuario ha sido insertado correctamente
-    }else{
-        // Error al insertar el usuario en la base de datos
-    }
+//var_dump($municipios);
+//exit();
+// Validar datos
 
-  }
 
 
 
@@ -46,7 +69,7 @@ $municipios = $modeloPerfilesUsuario->obtenerMunicipios();
         <div class="form-group">
           <label>Sexo:</label>
           <div class="sexoradio">
-            <input type="radio" id="masculino" name="sexo" value="1">
+            <input type="radio" id="masculino" name="sexo" value="1" required>
             <label for="masculino">Masculino</label>
             <input type="radio" id="femenino" name="sexo" value="2">
             <label for="femenino">Femenino</label>
@@ -70,57 +93,24 @@ $municipios = $modeloPerfilesUsuario->obtenerMunicipios();
             <?php } ?>
             <!-- Agrega más opciones de municipios aquí -->
           </datalist>
-          <script>
-            $(document).ready(function() {
-              $('#municipio').on('change', function() {
-                var selectedOption = $(this).val();
-                console.log("selected option: " + selectedOption);
-                
-                var datalist = $(this).attr('list');
-                var option = $('datalist#' + datalist + ' option').filter(function() {
-                  return $(this).text() === selectedOption;
-                });
-                
-                var selectedDataValue = option.data('value');
-                console.log("selected option id: " + selectedDataValue);
-                $(this).attr('data-value', selectedDataValue);
-              });
-
-              $('form').on('submit', function(e) {
-
-                var fechaNacimiento = new Date($('#fecha_nacimiento').val());
-                var fechaActual = new Date();
-                var minDate = new Date();
-                minDate.setFullYear(fechaActual.getFullYear() - 14); // Restar 14 años a la fecha actual
-                
-                if (fechaNacimiento > minDate) {
-                  // La fecha de nacimiento es menor a 14 años
-                  e.preventDefault(); // Evitar el envío del formulario
-                  $('#fecha_nacimiento').addClass('error'); // Marcar el campo con una clase de error
-                  // Mostrar mensaje de error
-                  $('#fecha_nacimiento_error').text('Debes tener al menos 14 años para registrarte.').show();
-                  return false;
-                }
-
-
-                var valueToSubmit = $('#municipio').data('value');
-                $(this).append('<input type="hidden" name="municipio-id" value="' + valueToSubmit + '">');
-              });
-            });
-          </script>
         </div>
-
         <div class="form-group">
           <label for="correo">Correo electrónico:</label>
           <input type="email" class="form-control" id="correo" name="correo" required>
+          <div id="correo_error" class="error-message" style="display: none;"></div>
+          <label for="correo_confirmar">Confirmar correo electrónico:</label>
+          <input type="email" class="form-control" id="correo_confirmar" name="correo_confirmar" required>
+          <div id="correo_confirmar_error" class="error-message" style="display: none;"></div>
         </div>
         <div class="form-group">
           <label for="password">Contraseña:</label>
           <input type="password" class="form-control" id="password" name="password" required>
+          <div id="password-strength" class="error-message">Mínimo 8 carácteres, 1 mayúscula, 1 minúscula, 1 número y un carácter especial </div>
         </div>
         <div class="form-group">
           <label for="password_confirm">Confirmar contraseña:</label>
           <input type="password" class="form-control" id="password_confirm" name="password_confirm" required>
+          <div id="password_confirm_error" class="error-message" style="display: none;"></div>
         </div>
         
         <button type="submit" class="btn btn-primary">Registrarse</button>
@@ -128,5 +118,147 @@ $municipios = $modeloPerfilesUsuario->obtenerMunicipios();
     </div>
   </div>
 </div>
+
+<script>
+  $(document).ready(function() {
+    var error_password = false;
+    $('#municipio').on('change', function() {
+      var selectedOption = $(this).val();
+      console.log("selected option: " + selectedOption);
+      
+      var datalist = $(this).attr('list');
+      var option = $('datalist#' + datalist + ' option').filter(function() {
+        return $(this).text() === selectedOption;
+      });
+      
+      var selectedDataValue = option.data('value');
+      console.log("selected option id: " + selectedDataValue);
+      $(this).attr('data-value', selectedDataValue);
+    });
+
+    $('#password').on('keyup', function() {
+      var password = $(this).val();
+
+      // Verificar la longitud de la contraseña
+      var lengthValid = password.length >= 8;
+
+      // Verificar si la contraseña contiene al menos una letra mayúscula
+      var uppercaseValid = /[A-Z]/.test(password);
+
+      // Verificar si la contraseña contiene al menos una letra minúscula
+      var lowercaseValid = /[a-z]/.test(password);
+
+      // Verificar si la contraseña contiene al menos un número
+      var numberValid = /\d/.test(password);
+
+      // Verificar si la contraseña contiene al menos un carácter especial
+      var specialCharValid = /[!@#$%^&*()\-=_+[\]{}|\\:;'"<>,.?/]/.test(password);
+
+      // Verificar si todas las condiciones se cumplen
+      var isPasswordValid = lengthValid && uppercaseValid && lowercaseValid && numberValid && specialCharValid;
+
+      // Actualizar la visualización de la fortaleza de la contraseña
+      if (isPasswordValid) {
+        $('#password-strength').text('Fuerte');
+        $('#password-strength').removeClass('weak').addClass('strong');
+        error_password=false;
+      } else {
+        $('#password-strength').text('Débil. Mínimo 8 carácteres, 1 mayúscula, 1 minúscula, 1 número y un carácter especial');
+        $('#password-strength').removeClass('strong').addClass('weak');
+        error_password=true;
+      }
+    });
+
+
+    $('form').on('submit', function(e) {
+      e.preventDefault(); // Evitar el envío del formulario
+      var error=false
+      //Comprobar edad
+      var fechaNacimiento = new Date($('#fecha_nacimiento').val());
+      var fechaActual = new Date();
+      var minDate = new Date();
+      minDate.setFullYear(fechaActual.getFullYear() - 14); // Restar 14 años a la fecha actual
+      if (fechaNacimiento > minDate) {
+        // La fecha de nacimiento es menor a 14 años
+        $('#fecha_nacimiento').addClass('error'); // Marcar el campo con una clase de error
+        // Mostrar mensaje de error
+        $('#fecha_nacimiento_error').text('Debes tener al menos 14 años para registrarte.').show();
+        error=true;
+      }else{
+        $('#fecha_nacimiento').removeClass('error');
+        $('#fecha_nacimiento_error').hide();
+      }
+
+      //Obtener id del municipio seleccionado
+      var valueToSubmit = $('#municipio').data('value');
+      $(this).append('<input type="hidden" name="municipio-id" value="' + valueToSubmit + '">');
+
+      //Comprobar email
+
+      var email = $('#correo').val();
+      var email_confirmar = $('#correo_confirmar').val();
+      if(email !== email_confirmar){
+        $('#correo_confirmar').addClass('error');
+        $('#correo_confirmar_error').text('El correo de confirmación no coincide con el correo ingresado.').show();;
+        error=true;
+      }else{
+        $('#correo_confirmar').removeClass('error');
+        $('#correo_confirmar_error').hide();
+      }
+
+      // Realizar la solicitud AJAX al servidor
+      $.ajax({
+        url: 'registro.php', // Ruta al archivo PHP que realizará la verificación en el servidor
+        type: 'POST',
+        data: { 
+          accion: 'verificar_email',
+          email: email
+          }, // Enviar el email al servidor
+
+        success: function(response) {
+          if (response !== 'no_existe') {
+            // El email ya está registrado, mostrar mensaje de error
+            $('#correo').addClass('error');
+            $('#correo_error').text('El email ya está registrado. Por favor, utiliza otro email.').show();
+            error=true;
+          } 
+        },
+
+        error: function() {
+          // Error en la solicitud AJAX
+          console.log('Error al verificar el email.');
+          error=true;
+        }
+      });
+
+
+
+      //Comprobar contraseñas
+      var password = $('#password').val();
+      var password_confirm = $('#password_confirm').val();
+      if(password !== password_confirm){
+        $('#password_confirm').addClass('error');
+        $('#password_confirm_error').text('Las contraseñas no coinciden.').show();;
+        error=true;
+      }else{
+        $('#password_confirm').removeClass('error');
+        $('#password_confirm_error').hide();
+      }
+      if(error_password){
+        error=true;
+      }
+      
+
+
+      if(error){
+        return false;
+      }else{
+        //$('form').unbind('submit').submit(); // Enviar el formulario nuevamente
+      }
+      
+      
+    });
+  });
+</script>
 
 <?php include 'vistas/footer.php'; ?>
